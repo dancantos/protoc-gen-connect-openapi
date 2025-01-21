3de3f4e51c031545e23c374fc2387af974da9836
dancantos <dancantos@gmail.com>
2025-01-22 10:34:32 +1100

    one more file to save

--- internal/converter/gnostic/convertions.go
+++ internal/converter/gnostic/convertions.go
@@ -3,13 +3,13 @@ package gnostic
 import (
 	"strconv"
 
+	"github.com/dancantos/protoc-gen-connect-openapi/internal/converter/util"
 	goa3 "github.com/google/gnostic/openapiv3"
 	base "github.com/pb33f/libopenapi/datamodel/high/base"
 	highv3 "github.com/pb33f/libopenapi/datamodel/high/v3"
 	v3 "github.com/pb33f/libopenapi/datamodel/high/v3"
 	"github.com/pb33f/libopenapi/orderedmap"
 	"github.com/pb33f/libopenapi/utils"
-	"github.com/sudorandom/protoc-gen-connect-openapi/internal/converter/util"
 	"gopkg.in/yaml.v3"
 )
 
